f8c1944d98ce53711630c9885d8ca551017059a1
Samuel Pohjolainen <samuel.pohjolainen@edu.bc.fi>
2018-10-01 12:29:33 +0300

    testi testi

---
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author s1800590
+ */
+public class JavaApplication8 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Tervetuloa pelaamaan Venttiä");
+        System.out.println("Ventti pelin säännöt");
+    }
+    
+}
