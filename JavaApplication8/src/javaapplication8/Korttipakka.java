b7109085139c30ebb7d3e7a9e7ef01bb7eddf084
Merja <s1801210@edu.bc.fi>
2018-10-03 11:39:24 +0300

    Tehty korttipakka ja Enum

---
+++ JavaApplication8/src/javaapplication8/Korttipakka.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author s1801210
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Korttipakka {
+    
+    public static void main(String[] args) {
+        List<String> kortti = new ArrayList<String>();
+        
+        kortti.add("Hertta 2"); //0
+        kortti.add("Hertta 3"); //1
+	kortti.add("Hertta 4"); //2
+        kortti.add("Hertta 5"); //3
+        kortti.add("Hertta 6"); //4
+	kortti.add("Hertta 7"); //5
+        kortti.add("Hertta 8"); //6
+        kortti.add("Hertta 9"); //7
+        kortti.add("Hertta 10"); //8
+        kortti.add("HErtta 11"); //9
+	kortti.add("Hertta 12"); //10
+        kortti.add("Hertta 13"); //11
+        kortti.add("Hertta Ässä"); //12
+        
+               
+	kortti.add("Ruutu 2"); //13
+        kortti.add("Ruutu 3"); //14
+        kortti.add("Ruutu 4"); //15
+        kortti.add("Ruutu 5"); //16
+        kortti.add("Ruutu 6"); //17
+	kortti.add("Ruutu 7"); //18
+        kortti.add("Ruutu 8"); //19
+        kortti.add("Ruutu 9"); //20
+        kortti.add("Ruutu 10"); //21
+        kortti.add("Ruutu 11"); //22
+	kortti.add("Ruutu 12"); //23
+        kortti.add("Ruutu 13"); //24
+        kortti.add("Ruutu ässä"); //25
+		
+        kortti.add("Risti 2"); //26
+        kortti.add("Risti 3"); //27
+        kortti.add("Risti 4"); //28
+        kortti.add("Risti 5"); //29
+        kortti.add("Risti 6"); //30
+	kortti.add("Risti 7"); //31
+        kortti.add("Risti 8"); //32
+        kortti.add("Risti 9"); //33
+        kortti.add("Risti 10"); //34
+        kortti.add("Risti 11"); //35
+	kortti.add("Risti 12"); //36
+        kortti.add("Risti 13"); //37
+        kortti.add("Risti ässä"); //38
+        
+        kortti.add("Pata 2"); //39
+        kortti.add("Pata 3"); //40
+        kortti.add("Pata 4"); //41
+        kortti.add("Pata 5"); //42
+        kortti.add("Pata 6"); //43
+        kortti.add("Pata 7"); //44
+        kortti.add("Pata 8"); //45
+        kortti.add("Pata 9"); //46
+        kortti.add("Pata 10"); //47
+        kortti.add("Pata 11"); //48
+        kortti.add("Pata 12"); //49
+        kortti.add("Pata 13"); //50
+        kortti.add("Pata ässä"); //51
+        
+            
+		                      
+                for(int i=0;i<kortti.size();i++){
+			System.out.println(kortti.get(i));
+                }
+		}
+    
+}
---
+++ JavaApplication8/src/javaapplication8/PakanEnum.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author s1801210
+ */
+public enum PakanEnum {
+    
+    Risti,
+    Ruutu,
+    Pata,
+    Assa,
+    
+}
